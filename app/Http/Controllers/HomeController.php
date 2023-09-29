ab41075748356531f3cb9c4d220bfee88a7577a8
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-29 16:50:53 +0530

    name completed in user site

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Name;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $name = Name::find(1);
+        return view('welcome', compact('name'));
     }
 }
