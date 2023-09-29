ba6773a244b63ecc49f8545c839dbcf72f2f3b50
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-29 12:54:26 +0530

    admin panel all operation completed

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,399 @@
+        <div class="left-side-menu">
+
+            <div class="h-100" data-simplebar>
+
+                <!-- User box -->
+                <div class="user-box text-center">
+                    <img src="assets/images/users/user-1.jpg" alt="user-img" title="Mat Helme"
+                        class="rounded-circle avatar-md">
+                    <div class="dropdown">
+                        <a href="javascript: void(0);" class="text-dark dropdown-toggle h5 mt-2 mb-1 d-block"
+                            data-bs-toggle="dropdown">Geneva Kennedy</a>
+                        <div class="dropdown-menu user-pro-dropdown">
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-user me-1"></i>
+                                <span>My Account</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-settings me-1"></i>
+                                <span>Settings</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-lock me-1"></i>
+                                <span>Lock Screen</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-log-out me-1"></i>
+                                <span>Logout</span>
+                            </a>
+
+                        </div>
+                    </div>
+                    <p class="text-muted">Admin Head</p>
+                </div>
+
+                <!--- Sidemenu -->
+                <div id="sidebar-menu">
+
+                    <ul id="side-menu">
+
+
+                        <li>
+                            <a href="{{ route('admin-dashboard') }}">
+                                <i class="mdi mdi-view-dashboard-outline"></i>
+                                <span> Dashboard </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#sidebarEcommerce" data-bs-toggle="collapse">
+                                <i class="mdi mdi-cart-outline"></i>
+                                <span> Ecommerce </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarEcommerce">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="ecommerce-dashboard.html">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="ecommerce-products.html">Products</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarCrm" data-bs-toggle="collapse">
+                                <i class="mdi mdi-account-multiple-outline"></i>
+                                <span> CRM </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarCrm">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="crm-dashboard.html">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="crm-contacts.html">Contacts</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarEmail" data-bs-toggle="collapse">
+                                <i class="mdi mdi-email-multiple-outline"></i>
+                                <span> Email </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarEmail">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="email-inbox.html">Inbox</a>
+                                    </li>
+                                    <li>
+                                        <a href="email-read.html">Read Email</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+
+
+                        <li>
+                            <a href="#sidebarProjects" data-bs-toggle="collapse">
+                                <i class="mdi mdi-briefcase-check-outline"></i>
+                                <span> Projects </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarProjects">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="project-list.html">List</a>
+                                    </li>
+                                    <li>
+                                        <a href="project-detail.html">Detail</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarTasks" data-bs-toggle="collapse">
+                                <i class="mdi mdi-clipboard-multiple-outline"></i>
+                                <span> Tasks </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarTasks">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="task-list.html">List</a>
+                                    </li>
+                                    <li>
+                                        <a href="task-details.html">Details</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarContacts" data-bs-toggle="collapse">
+                                <i class="mdi mdi-book-account-outline"></i>
+                                <span> Contacts </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarContacts">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="contacts-list.html">Members List</a>
+                                    </li>
+                                    <li>
+                                        <a href="contacts-profile.html">Profile</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarTickets" data-bs-toggle="collapse">
+                                <i class="mdi mdi-lifebuoy"></i>
+                                <span> Tickets </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarTickets">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="tickets-list.html">List</a>
+                                    </li>
+                                    <li>
+                                        <a href="tickets-detail.html">Detail</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+
+
+
+                        <li>
+                            <a href="#sidebarAuth" data-bs-toggle="collapse">
+                                <i class="mdi mdi-account-circle-outline"></i>
+                                <span> Auth Pages </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarAuth">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="auth-login.html">Log In</a>
+                                    </li>
+                                    <li>
+                                        <a href="auth-login-2.html">Log In 2</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarExpages" data-bs-toggle="collapse">
+                                <i class="mdi mdi-text-box-multiple-outline"></i>
+                                <span> Extra Pages </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarExpages">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="pages-starter.html">Starter</a>
+                                    </li>
+                                    <li>
+                                        <a href="pages-timeline.html">Timeline</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarLayouts" data-bs-toggle="collapse">
+                                <i class="mdi mdi-cellphone-link"></i>
+                                <span class="badge bg-blue float-end">New</span>
+                                <span> Layouts </span>
+                            </a>
+                            <div class="collapse" id="sidebarLayouts">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="layouts-horizontal.html">Horizontal</a>
+                                    </li>
+                                    <li>
+                                        <a href="layouts-detached.html">Detached</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+
+                        <li>
+                            <a href="#sidebarBaseui" data-bs-toggle="collapse">
+                                <i class="mdi mdi-black-mesa"></i>
+                                <span> Base UI </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarBaseui">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="ui-buttons.html">Buttons</a>
+                                    </li>
+                                    <li>
+                                        <a href="ui-cards.html">Cards</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarExtendedui" data-bs-toggle="collapse">
+                                <i class="mdi mdi-layers-outline"></i>
+                                <span class="badge bg-info float-end">Hot</span>
+                                <span> Extended UI </span>
+                            </a>
+                            <div class="collapse" id="sidebarExtendedui">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="extended-nestable.html">Nestable List</a>
+                                    </li>
+                                    <li>
+                                        <a href="extended-range-slider.html">Range Slider</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="widgets.html">
+                                <i class="mdi mdi-gift-outline"></i>
+                                <span> Widgets </span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarIcons" data-bs-toggle="collapse">
+                                <i class="mdi mdi-bullseye"></i>
+                                <span> Icons </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarIcons">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="icons-material-symbols.html">Material Symbols Icons</a>
+                                    </li>
+                                    <li>
+                                        <a href="icons-two-tone.html">Two Tone Icons</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarForms" data-bs-toggle="collapse">
+                                <i class="mdi mdi-bookmark-multiple-outline"></i>
+                                <span> Forms </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarForms">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="forms-elements.html">General Elements</a>
+                                    </li>
+                                    <li>
+                                        <a href="forms-advanced.html">Advanced</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarTables" data-bs-toggle="collapse">
+                                <i class="mdi mdi-table"></i>
+                                <span> Tables </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarTables">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="tables-basic.html">Basic Tables</a>
+                                    </li>
+                                    <li>
+                                        <a href="tables-datatables.html">Data Tables</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarCharts" data-bs-toggle="collapse">
+                                <i class="mdi mdi-poll"></i>
+                                <span> Charts </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarCharts">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="charts-apex.html">Apex Charts</a>
+                                    </li>
+                                    <li>
+                                        <a href="charts-flot.html">Flot Charts</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#sidebarMaps" data-bs-toggle="collapse">
+                                <i class="mdi mdi-map-outline"></i>
+                                <span> Maps </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarMaps">
+                                <ul class="nav-second-level">
+                                    <li>
+                                        <a href="maps-google.html">Google Maps</a>
+                                    </li>
+                                    <li>
+                                        <a href="maps-vector.html">Vector Maps</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </div>
+                <!-- End Sidebar -->
+
+                <div class="clearfix"></div>
+
+            </div>
+            <!-- Sidebar -left -->
+
+        </div>
---
+++ routes/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+Route::get('/test', function () {
+    echo "Abhiram";
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/login', [AdminController::class, 'login'])->name('admin-login');
+    Route::post('/login', [AdminController::class, 'loginPost'])->name('admin-login-post');
+
+    Route::group(['middleware' => 'auth:admin'], function () {
+        Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin-dashboard');
+        Route::get('/logout', [Admincontroller::class, 'adminLogout'])->name('admin-logout');
+        Route::get('/profile', [Admincontroller::class, 'adminProfile'])->name('admin-profile');
+        Route::post('/profile/update', [AdminController::class, 'adminProfileUpdate'])->name('admin-profile-update');
+        Route::get('/change/password', [Admincontroller::class, 'changePassword'])->name('admin-change-password');
+
+        Route::post('/update/password', [AdminController::class, 'updatePassword'])->name('admin-password-update');
+
+
+    });
+});
